c49c8bbb96413246ece6a55fd811289787b794ab
DeepSaha25 <ideepsaha25@gmail.com>
2025-12-12 23:32:04 +0530

    initials

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const experienceData = [
+  {
+    id: 1,
+    title: "Learned HTML",
+    date: "(June 2025)",
+    desc: "Started learning Html css from youtube and builing logics and understanding core concepts and structures.",
+    img: "/img/html.png"
+  },
+  {
+    id: 2,
+    title: "Learned CSS",
+    date: "(July 2025)",
+    desc: "Started learning CSS from youtube and builing logics and understanding core concepts and structures.",
+    img: "/img/css.png"
+  },
+  {
+    id: 3,
+    title: "Learned Javascript basics",
+    date: "(August 2025)",
+    desc: "Started learning Js basics from youtube and builing logics and understanding core concepts and structures.",
+    img: "/img/js.png"
+  },
+  {
+    id: 4,
+    title: "First Hackathon - CodeSprint 2.0",
+    date: "(Sept 2025)",
+    desc: "Participated in my first hackathon with less than 1 month in college and succesfully qualified.",
+    img: "/img/hack.png"
+  },
+  {
+    id: 5,
+    title: "Hacktoberfest Supercontributor",
+    date: "(Oct 2025)",
+    desc: "Participated in Hactoberfest and succesfully dwelved into world of open source contribution.",
+    img: "/img/hackoct.png"
+  }
+];
+
+export const projectData = [
+  {
+    id: 1,
+    img: "/img/swasthi.png",
+    link: "/"
+  },
+  {
+    id: 2,
+    img: "/img/tunemate.png",
+    link: "https://deepsaha25.github.io/TuneMate/"
+  },
+  {
+    id: 3,
+    img: "/img/todo.png",
+    link: "https://deepsaha25.github.io/ToDo-List/"
+  }
+];
